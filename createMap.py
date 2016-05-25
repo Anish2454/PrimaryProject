bb6f74590518c2b764642126e4b0dd983bfbaa8e
Anish Shenoy <shenoy.anish.227@gmail.com>
2016-05-25 14:02:26 -0400

    Added file splicing
    organizes dem data into a dictionary

--- createMap.py
+++ createMap.py
@@ -9,3 +9,23 @@ trace = {"type": "choropleth", "locations": ["NY", "MN", "VT", "FL", "OH", "CA"]
 lyt = {"geo": {"scope":'usa'}, "title":"Anish's Map"}
 map = go.Figure(data = [trace], layout=lyt)
 py.plot(map, filename="my plot")
+
+def openFile():
+    f = open("demdata.csv", "rU")
+    s = f.read()
+    f.close()
+    return s
+
+def organize():
+    s = openFile()
+    stateByState = s.split("\n")
+    lst = [i.split(",") for i in stateByState]
+    keys = lst[0]
+    mD = {}
+    for i in lst[1:-1]:
+       d = {}
+       for n in range(len(i)):
+           if n < len(keys):
+               d[keys[n]] = i[n]
+       mD[i[0]] = d
+    print mD
