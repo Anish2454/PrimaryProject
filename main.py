835237eb6f434df14eccb1e9675baada6e63f9fb
Anish2454 <shenoy.anish.227@gmail.com>
2016-05-29 10:30:29 -0400

    Read CSV and Populate Map

--- main.py
+++ main.py
@@ -5,6 +5,91 @@ import json
 
 cgitb.enable()
 
+us_state_abbrev = {
+    'Alabama': 'AL',
+    'Alaska': 'AK',
+    'Arizona': 'AZ',
+    'Arkansas': 'AR',
+    'California': 'CA',
+    'Colorado': 'CO',
+    'Connecticut': 'CT',
+    'Delaware': 'DE',
+    'District of Columbia': 'DC',
+    'Florida': 'FL',
+    'Georgia': 'GA',
+    'Hawaii': 'HI',
+    'Idaho': 'ID',
+    'Illinois': 'IL',
+    'Indiana': 'IN',
+    'Iowa': 'IA',
+    'Kansas': 'KS',
+    'Kentucky': 'KY',
+    'Louisiana': 'LA',
+    'Maine': 'ME',
+    'Maryland': 'MD',
+    'Massachusetts': 'MA',
+    'Michigan': 'MI',
+    'Minnesota': 'MN',
+    'Mississippi': 'MS',
+    'Missouri': 'MO',
+    'Montana': 'MT',
+    'Nebraska': 'NE',
+    'Nevada': 'NV',
+    'New Hampshire': 'NH',
+    'New Jersey': 'NJ',
+    'New Mexico': 'NM',
+    'New York': 'NY',
+    'North Carolina': 'NC',
+    'North Dakota': 'ND',
+    'Ohio': 'OH',
+    'Oklahoma': 'OK',
+    'Oregon': 'OR',
+    'Pennsylvania': 'PA',
+    'Rhode Island': 'RI',
+    'South Carolina': 'SC',
+    'South Dakota': 'SD',
+    'Tennessee': 'TN',
+    'Texas': 'TX',
+    'Utah': 'UT',
+    'Vermont': 'VT',
+    'Virginia': 'VA',
+    'Washington': 'WA',
+    'West Virginia': 'WV',
+    'Wisconsin': 'WI',
+    'Wyoming': 'WY',
+}
+
+def openFile(filename):
+    f = open(filename, "rU")
+    s = f.read()
+    f.close()
+    return s
+
+def organize():
+    s = openFile("demdata.csv")
+    stateByState = s.split("\n")
+    lst = [i.split(",") for i in stateByState]
+    keys = lst[0]
+    mD = {}
+    for i in lst[1:-1]:
+       d = {}
+       for n in range(len(keys)):
+           if n < len(i):
+               d[keys[n]] = i[n]
+           else:
+               d[keys[n]] = ""
+       if d["Bernie Delegates"] != "" and d["Clinton Delegates"] != "":
+           if max(int(d["Bernie Delegates"]), int(d["Clinton Delegates"])) == int(d["Bernie Delegates"]):
+               d["Winner"] = "Bernie"
+           else:
+               d["Winner"] = "Clinton"
+       if i[0] in us_state_abbrev:
+           mD[us_state_abbrev.get(i[0], "")] = d
+       else:
+           mD[i[0]] = d
+
+    return mD
+
 def convertListToJs(list):
     finalString = "["
     for i in list:
@@ -12,10 +97,53 @@ def convertListToJs(list):
     finalString += "]"
     return finalString
 
-locations = ["NY", "MN", "VT", "FL", "OH", "CA"]
-values = [1,0,0,1,1,0]
+def openFile(filename):
+    f = open(filename, "rU")
+    s = f.read()
+    f.close()
+    return s
+
+def organize():
+    s = openFile("demdata.csv")
+    stateByState = s.split("\n")
+    lst = [i.split(",") for i in stateByState]
+    keys = lst[0]
+    mD = {}
+    for i in lst[1:-1]:
+       d = {}
+       for n in range(len(keys)):
+           if n < len(i):
+               d[keys[n]] = i[n]
+           else:
+               d[keys[n]] = ""
+       if d["Bernie Delegates"] != "" and d["Clinton Delegates"] != "":
+           if max(int(d["Bernie Delegates"]), int(d["Clinton Delegates"])) == int(d["Bernie Delegates"]):
+               d["Winner"] = "Bernie"
+           else:
+               d["Winner"] = "Clinton"
+       else:
+           d["Winner"] = "None"
+       if i[0] in us_state_abbrev:
+           mD[us_state_abbrev.get(i[0], "")] = d
+       else:
+           mD[i[0]] = d
+            
+    return mD
+
+def locationsAndValues():
+    winnerDict = {"Bernie":0, "Clinton":1, "None":0.5}
+    d = organize()
+    locations =[]
+    values = []
+    for i in d:
+        if len(i) == 2:
+            if "Winner" in d[i]:
+                locations.append(i)
+                values.append(winnerDict[d[i]["Winner"]])
+    return [locations, values]
 
 HTML_HEADER = 'Content-type: text/html\n\n'
+
 Top_HTML = '''
 <html>
 <head>
@@ -29,35 +157,38 @@ Top_HTML = '''
 
 Bottom_HTML = "</script></body></html>"
 
-js = '''
-var chartDiv = document.getElementById('chart-div');
-var data = [{
+
+def main():
+    locationsValues = locationsAndValues()
+
+    js = '''
+    var chartDiv = document.getElementById('chart-div');
+    var data = [{
     type: "choropleth",
-    locations: ''' + convertListToJs(locations) + "," + '''
+    locations: ''' + convertListToJs(locationsValues[0]) + "," + '''
     locationmode: "USA-states",
     colorscale: [[0,"green"], [1,"blue"]],
     showscale: false,
     hoverinfo: "location",
-    z: ''' + convertListToJs(values) + '''
-  }];
-  var layout = {
+    z: ''' + convertListToJs(locationsValues[1]) + '''
+    }];
+    var layout = {
     title: "Anish's Map",
     autosize: false,
     width: window.innerWidth,
     height: window.innerHeight,
     geo: {
-      scope: "usa"
+      scope: "usa",
+      showlakes: true,
+      lakecolor: 'cyan'
     }
-  };
-  Plotly.plot(chartDiv, data, layout);
+    };
+    Plotly.plot(chartDiv, data, layout);
 
-  chartDiv.on("plotly_click", function(data){
-    window.open("http://bert.stuy.edu/pbrooks/ml2/SampleQuestionsAnswers.py?CheckArithmetic=ON&FirstNum=5&Operator=%2F&SecondNum=12&CheckString=ON&TheString=%22Anish%22&RadioString=Lower&TheSubmitButton=Get+the+answers");
-  });
+    chartDiv.on("plotly_click", function(data){
+    alert(data.points[0].location);
+    });
 '''
-
-
-def main():
     print(HTML_HEADER)
     print(Top_HTML)
     print(js)
