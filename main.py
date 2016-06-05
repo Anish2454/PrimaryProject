7312bbd07928c3e36ffbb920619b8dd15a079a30
Anish2454 <shenoy.anish.227@gmail.com>
2016-06-04 22:58:47 -0400

    Fixed errors in csv, Design overhaul

--- main.py
+++ main.py
@@ -126,10 +126,42 @@ Top_HTML = '''
 <head>
 <title>Sample Python Forms-responder</title>
 <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
+<link href='https://fonts.googleapis.com/css?family=Playfair+Display' rel='stylesheet' type='text/css'>
 </head>
 <body>
-<div id="chart-div" height="100%" width="100%"></div>
-<div id="chart-div-2"></div>
+<style>
+.nav{
+	border:1px solid #ccc;
+    border-width:1px 0;
+    list-style:none;
+    margin:0;
+    padding:0;
+    text-align:center;
+}
+.nav li{
+    display:inline;
+}
+.nav a{
+    display:inline-block;
+    padding:10px;
+    color: #008FD5;
+    text-decoration: none;
+    font-size: 17px;
+    line-height: 26px;
+    font-family: AtlasGrotesk, 'Helvetica Neue', Helvetica, Arial, sans-serif;
+}
+
+.nav a:hover {
+  font-size: 30px;
+  }
+
+</style>
+<ul class = "nav">
+  <li><a href="http://marge.stuy.edu/~anish.shenoy/Final_Project/main.py">Home</a></li>
+  <li><a href="https://www.washingtonpost.com/graphics/politics/2016-election/primaries/schedule/">Primary Dates</a></li>
+  <li><a href="https://votesmart.org/education/presidential-primary#.V1NTW7grKUk">Information on Primaries</a></li>
+
+</ul>
 '''
 
 Bottom_HTML = "</body></html>"
@@ -193,21 +225,32 @@ def displayMap(masterDict):
     type: "choropleth",
     locations: ''' + convertListToJs(locationsValues[0]) + "," + '''
     locationmode: "USA-states",
-    colorscale: [[0,"green"], [1,"blue"]],
+    colorscale: [[0,"rgb(102, 187, 106)"], [1,"rgb(21, 101, 192)"]],
     showscale: false,
     hoverinfo: "location",
-    z: ''' + convertListToJs(locationsValues[1]) + '''
+    z: ''' + convertListToJs(locationsValues[1]) + ''',
+    marker: {
+    line: {
+    width: 2,
+    color: "white"
+    }
+    }
     }];
     var layout = {
-    title: "Anish's Map",
     autosize: false,
     width: window.innerWidth,
-    height: window.innerHeight,
+    height: window.innerHeight - 100,
     geo: {
       scope: "usa",
       showlakes: true,
       lakecolor: 'cyan'
-    }
+    },
+    font: {
+       family: 'Playfair Display, serif',
+       size: 24,
+       color: "Black",
+       bold: true
+       }
     };
     Plotly.plot(chartDiv, data, layout);
 
@@ -215,40 +258,44 @@ def displayMap(masterDict):
     window.open("main.py?state=" + data.points[0].location);
     });
     '''
+    print('<div id="page">')
+    print('<center><div id="header"><h1> <font size=10>2016 Democratic Primaries</font></h1><p>With the upcoming elections predicted to be one of the most influential elections in history, it is imperative that we have some of the most informed voters in history. This map is updated daily with results from the democratic primaries; click on a state for more information about the results from that state.</p></div></center>')
+    style = '''
+    <style>
+    #header {
+        position: relative;
+    }
+    </style>
+    '''
+    print('<center><div id="chart-div"></div></center>')
+    print('</div>')
+    print(style)
     print("<script>")
     print(js)
     print("</script>")
 
-def displayStatePage(state, masterDict):
-    if state in masterDict:
-        stateInfo = masterDict[state]
-    if "Bernie Delegates" in stateInfo and "Clinton Delegates" in stateInfo:
-        xBernie = [stateInfo["Bernie Delegates"]]
-        xClinton = [stateInfo["Clinton Delegates"]]
-    else:
-        xBernie = 0
-        xClinton = 0
+def plotDelgateGraph(bernieDel, clintonDel, state, divName):
     js = '''
     var trace1 = {
-        x: ''' + convertListToJs(xBernie) + "," + '''
+        x: ''' + convertListToJs([bernieDel]) + "," + '''
         y: ["Sanders"],
         name: "Bernie Sanders",
         orientation: 'h',
         type: 'bar',
         marker: {
-            color: 'rgba(55,128,191,0.6)',
+            color: 'rgba(102, 187, 106, .5)',
             width: 1
         }
     };
 
     var trace2 = {
-        x: ''' + convertListToJs(xClinton) +',' + '''
+        x: ''' + convertListToJs([clintonDel]) +',' + '''
         y: ["Clinton"],
         name: 'Hillary Cinton',
         orientation: 'h',
         type: 'bar',
         marker: {
-            color: "Blue",
+            color: "rgba(21, 101, 192, 0.5)",
             width: 1
         }
     };
@@ -257,20 +304,154 @@ def displayStatePage(state, masterDict):
 
     var layout = {
         title: 'The ''' + states[state] + ''' Delegate Count',
-        width: (window.innerWidth / 2),
-        height: (window.innerHeight / 2),
+        width: 1024,
+        height: 350,
         font: {
-            family: 'Arial',
-            size: 24,
-            color: "Black"
+           family: 'Playfair Display, serif',
+           size: 20,
+           color: 666
+           }
+    };
+
+    Plotly.newPlot("''' + divName + '''", data, layout);
+    '''
+    return js
+
+def plotDelgatePie(bernieDel, clintonDel, state, divName):
+    values = [bernieDel, clintonDel]
+    js = '''
+    var data = [{
+      values: ''' + convertListToJs(values) + ',' + '''
+      labels: ['Bernie Sanders', 'Hillary Clinton'],
+      type: 'pie',
+      marker :{
+        colors: ['rgba(102, 187, 106, .5)', 'rgba(21, 101, 192, 0.5)']
+        }
+    }];
+
+    var layout = {
+     title: 'The ''' + states[state] + ''' Delegate Count by %',
+     height: 400,
+     width: 1000,
+     font: {
+        family: 'Playfair Display, serif',
+        size: 20,
+        color: "Black"
+        },
+    autoexpand: false
+    };
+    Plotly.newPlot(''' + "'" + divName + "'" + ''', data, layout);
+    '''
+    return js
+
+def plotVoteGraph(bernieVotes, clintonVotes, state, divName):
+    js = '''
+    var trace1 = {
+        x: ''' + convertListToJs([bernieVotes]) + "," + '''
+        y: ["Sanders"],
+        name: "Bernie Sanders",
+        orientation: 'h',
+        type: 'bar',
+        marker: {
+            color: 'rgba(102, 187, 106, .5)',
+            width: 1
         }
     };
 
-    Plotly.newPlot('chart-div', data, layout);
+    var trace2 = {
+        x: ''' + convertListToJs([clintonVotes]) +',' + '''
+        y: ["Clinton"],
+        name: 'Hillary Cinton',
+        orientation: 'h',
+        type: 'bar',
+        marker: {
+            color: "rgba(21, 101, 192, 0.5)",
+            width: 1
+        }
+    };
+
+    var data = [trace1, trace2];
+
+    var layout = {
+        title: 'The ''' + states[state] + ''' Popular Vote',
+        width: 1024,
+        height: 350,
+        font: {
+           family: 'Playfair Display, serif',
+           size: 20,
+           color: 666
+           }
+    };
+
+    Plotly.newPlot("''' + divName + '''", data, layout);
     '''
-    print("<script>")
-    print(js)
-    print("</script")
+    return js
+
+def plotVotePieChart(bernieVotes, clintonVotes, state, divName):
+    values = [bernieVotes, clintonVotes]
+    js = '''
+    var data = [{
+      values: ''' + convertListToJs(values) + ',' + '''
+      labels: ['Bernie Sanders', 'Hillary Clinton'],
+      type: 'pie',
+      marker :{
+        colors: ['rgba(102, 187, 106, .5)', 'rgba(21, 101, 192, 0.5)']
+        }
+    }];
+
+    var layout = {
+     title: 'The ''' + states[state] + ''' Popular Vote by %',
+     height: 400,
+     width: 1000,
+     font: {
+        family: 'Playfair Display, serif',
+        size: 20,
+        color: "Black"
+        },
+    autoexpand: false
+    };
+    Plotly.newPlot(''' + "'" + divName + "'" + ''', data, layout);
+    '''
+    return js
+
+def stateNotVoted(state):
+    print(states[state]  + " Has Not Voted Yet")
+
+def displayStatePage(state, masterDict):
+    print('<center><div id="delegate-div">')
+    print('<div id="delegate-horiz"></div>')
+    print('<div id="delegate-pie"></div>')
+    print('</div></center>')
+    print('<center><div id="pop-div">')
+    print('<div id="pop-horiz"></div>')
+    print('<div id="pop-pie"></div>')
+    print('</div></center>')
+    style = '''
+    <style>
+    #delegate-pie, #delegate-horiz, #pop-pie, #pop-horiz {
+        display: inline-block;
+    }
+    </style>
+    '''
+    print(style)
+    if state in masterDict:
+        stateInfo = masterDict[state]
+    if stateInfo["Bernie Delegates"] != "" and stateInfo["Clinton Delegates"] != "":
+        bernieDel = int(stateInfo["Bernie Delegates"])
+        clintonDel = int(stateInfo["Clinton Delegates"])
+        print("<script>")
+        print(plotDelgateGraph(bernieDel, clintonDel, state, "delegate-horiz"))
+        print(plotDelgatePie(bernieDel, clintonDel, state, "delegate-pie"))
+        print("</script>")
+    else:
+        stateNotVoted(state)
+    if stateInfo["Bernie Votes"] != "" and stateInfo["Clinton Votes"] != "":
+        bernieVotes = int(stateInfo["Bernie Votes"])
+        clintonVotes = int(stateInfo["Clinton Votes"])
+        print("<script>")
+        print(plotVoteGraph(bernieVotes, clintonVotes, state, "pop-horiz"))
+        print(plotVotePieChart(bernieVotes, clintonVotes, state, "pop-pie"))
+        print("</script>")
 
 def main():
     masterDict = organize()
